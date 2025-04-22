65221bd59ba0b02c7f16a28c2ad9cf3012c58fd6
xyhgsy <1840113472@qq.com>
2025-04-22 13:38:09 +0800

    创建初始main.cc

---
+++ main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void my_sort(int arr[],int size)
+{
+  
+}
+
+int main()
+{
+  int arr[5] = {1,2,3,4,5};
+  int size = 5;
+  my_sort(arr,5);
+  return 0;
+}
