216503f8639338a03826f8de4cce28486a6bfe28
xyhgsy <1840113472@qq.com>
2025-04-22 13:54:43 +0800

    排序成功

--- main.cc
+++ main.cc
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void my_sort(int arr[],int size)
+void my_sort(int arr[],int sizes)
 {
   
+  printf("排序成功");
 }
 
 int main()
 {
   int arr[5] = {1,2,3,4,5};
-  int size = 5;
+  int sizes = 5;
   my_sort(arr,5);
   return 0;
 }
