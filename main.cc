b368ea1b59ea47f9ca2df48f94c1f431eee7ddec
xyhgsy <1840113472@qq.com>
2025-04-22 20:11:27 +0800

    branch test

--- main.cc
+++ main.cc
@@ -1,17 +1,2 @@
-#include <iostream>
-using namespace std;
+Creating a new branch is quick.
 
-void my_sort(int arr[],int sizes)
-{
-  
-  printf("排序成功");
-  "学习分支"
-}
-
-int main()
-{
-  int arr[5] = {1,2,3,4,5};
-  int sizes = 5;
-  my_sort(arr,5);
-  return 0;
-}
